432f265e79232dcd1bc49072c07dc2ab96e00b9e
Hafeez <izzhafeez@gmail.com>
2022-08-28 18:21:41 +0800

    Settings.py

--- MeetupMaker/settings.py
+++ MeetupMaker/settings.py
@@ -51,11 +51,13 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CSRF_COOKIE_SECURE = False
 
 ROOT_URLCONF = 'MeetupMaker.urls'
 
